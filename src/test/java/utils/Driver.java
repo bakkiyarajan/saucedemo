805bec6a23a599ae60234f6f6c4ce19a3f05e228
bakkiya <bakkiyaqa@gmail.com>
2026-07-24 19:35:12 +0100

    util file updated

--- src/test/java/utils/Driver.java
+++ src/test/java/utils/Driver.java
@@ -1,21 +1,43 @@
 package utils;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeOptions;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Driver {
 
     protected static WebDriver driver = getDriver();
 
     protected static WebDriver getDriver() {
+
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+
+        ChromeOptions options = new ChromeOptions();
+
+        Map<String, Object> prefs = new HashMap<>();
+        prefs.put("credentials_enable_service", false);
+        prefs.put("profile.password_manager_enabled", false);
+
+        options.setExperimentalOption("prefs", prefs);
+
+        options.addArguments("--disable-save-password-bubble");
+        options.addArguments("--disable-features=PasswordLeakDetection");
+        options.addArguments("--disable-notifications");
+        options.addArguments("--guest");
+        driver = new ChromeDriver(options);
+
         driver.manage().deleteAllCookies();
         driver.manage().window().maximize();
+
         Runtime.getRuntime().addShutdownHook(new Thread("Driver shutdown thread") {
+            @Override
             public void run() {
-                driver.quit();
+                if (driver != null) {
+                    driver.quit();
+                }
             }
         });
 
